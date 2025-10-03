35da0c4467355a236e2214ed5ddec0a522097069
MAHossain1 <mahpro110@gmail.com>
2025-10-03 12:47:55 +0600

    modularized whole ec2 provision and working perfectly

--- main.tf
+++ main.tf
@@ -17,98 +17,15 @@ module "myapp-subnet" {
 }
     
 
-
-resource "aws_default_security_group" "default-sg" {
+module "myapp-server" {
+    source = "./modules/webserver"
     vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-        Name: "${var.env_prefix}-default-sg"
-    }
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = []
-    } 
+    my_ip = var.my_ip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    public_key_location = var.public_key_location
+    instance_type = var.instance_type
+    subnet_id = module.myapp-subnet.subnet.id
+    avail_zone = var.avail_zone
 }
 
-data "aws_ami" "latest-amazon-linux-image" {
-    most_recent = true
-    owners = ["amazon"]
-
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
-
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
-
-resource "aws_key_pair" "ssh-key" {
-    key_name = "server-key"
-    public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-    ami                    = data.aws_ami.latest-amazon-linux-image.id
-    instance_type          = var.instance_type
-    subnet_id              = module.myapp-subnet.subnet.id
-    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-    associate_public_ip_address = true
-    key_name               = aws_key_pair.ssh-key.key_name
-
-#   user_data = file("entry-script.sh")
-
-    connection {
-        type       = "ssh"
-        host       = self.public_ip
-        user       = "ec2-user"
-        private_key = file(var.private_key_location)
-    }
-
-    provisioner "file" {
-        source      = "entry-script.sh"
-        destination = "/home/ec2-user/entry-script-on-ec2.sh"
-    }
-
-    # provisioner "remote-exec" {
-    #     inline = [
-    #         "export ENV=dev",
-    #         "mkdir newdir"
-    #     ]
-    # }
-
-    # provisioner "remote-exec" {
-    #     script = file("entry-script-on-ec2.sh")
-    # }
-
-    # provisioner "local-exec" {
-    #     command = "echo 'Instance created with public IP: ${self.public_ip}'"
-    # }
-
-  tags = {
-    Name = "${var.env_prefix}-myapp-terraform-server"
-  }
-}
